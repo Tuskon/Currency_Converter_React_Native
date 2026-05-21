7e8aa14669e09ef7def2080dbd1a9b4fda8b8733
José Luiz Rodrigues dos Santos <xgjlrs@gmail.com>
2026-05-21 17:10:38 -0400

    [FEAT]-[Currency Converter Logic]-[Adding conversion logic]

--- src/utils/requests_http.tsx
+++ src/utils/requests_http.tsx
@@ -5,19 +5,12 @@ axios.defaults.withCredentials = true;
 
 export const Get = async (url: string, params?: any, countryAPI?: boolean) => {
     try {
-
-    console.log(url)
-
-    console.log(params)
-
-    console.log(countryAPI)
         let API = countryAPI ? API_COUNTRYS : API_CURRENCYS
         const headers = { 'Content-Type': 'application/json' };
         const response = await axios.get(`${API}${url}`, { headers, params });
         console.log(`${API}${url}`)
         return response.data;
     } catch (error: any) {
-        console.log(error)
         throw {
             status: error?.response?.status || 500,
             data: error?.response?.data || 'Erro desconhecido',
